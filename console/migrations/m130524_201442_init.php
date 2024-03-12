8c119a854e9dbdfe50667a89ccdf3a2c2ed6f947
DoniyorY <87862961+DoniyorY@users.noreply.github.com>
2024-03-12 11:05:44 +0500

    fixing User Migration

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -14,12 +14,13 @@ class m130524_201442_init extends Migration
 
         $this->createTable('{{%user}}', [
             'id' => $this->primaryKey(),
+            'fullname'=>$this->string()->notNull(),
             'username' => $this->string()->notNull()->unique(),
+            'role_id'=>$this->integer(),
             'auth_key' => $this->string(32)->notNull(),
             'password_hash' => $this->string()->notNull(),
             'password_reset_token' => $this->string()->unique(),
             'email' => $this->string()->notNull()->unique(),
-
             'status' => $this->smallInteger()->notNull()->defaultValue(10),
             'created_at' => $this->integer()->notNull(),
             'updated_at' => $this->integer()->notNull(),
