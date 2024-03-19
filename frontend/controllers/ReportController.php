7ddf875a5a572f76e27a558f95ffea3976d8d485
DoniyorY <87862961+DoniyorY@users.noreply.github.com>
2024-03-19 13:49:18 +0500

    Start Reports

--- frontend/controllers/ReportController.php
+++ frontend/controllers/ReportController.php
@@ -2,6 +2,7 @@
 
 namespace frontend\controllers;
 
+use common\models\Clients;
 use common\models\Sale;
 use common\models\SaleItem;
 use yii\data\ActiveDataProvider;
@@ -25,6 +26,7 @@ class ReportController extends Controller
                         [
                             'actions' => [
                                 'clients',
+                                'client-items'
                             ],
                             'allow' => true,
                             'roles' => ['@'],
@@ -37,15 +39,42 @@ class ReportController extends Controller
 
     public function actionClients()
     {
-        $query=SaleItem::find()->joinWith('sale')->where(['sale.is_deleted'=>0]);
-        $dataProvider= new ActiveDataProvider([
-            'query'=>$query,
+        //$model=Clients::findAll(['is_deleted'=>0]);
+        $model = new ActiveDataProvider([
+            'query' => Clients::find()->where(['is_deleted' => 0])->orderBy(['fullname' => 4])
         ]);
-
-
-        return $this->render('client',[
-            'model'=>$dataProvider->models
+        if (\Yii::$app->request->get('Client')) {
+            $get = $_GET['Client'];
+            $model->query->andFilterWhere(['like', 'fullname', $get['fullname']])
+                ->andFilterWhere(['like', 'phone', $get['phone']]);
+        }
+        return $this->render('client', [
+            'model' => $model->models,
         ]);
+    }
 
+    public function actionClientItems($id)
+    {
+        $client = Clients::findOne(['token' => $id]);
+        $query = SaleItem::find()
+            ->joinWith('sale')
+            ->where(['sale.is_deleted' => 0, 'sale.client_id' => $client->id])
+            ->orderBy(['id' => 3]);
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+        if (\Yii::$app->request->get('Sale')) {
+            $get = $_GET['Sale'];
+            $begin = strtotime($get['begin']);
+            $end = strtotime($get['end']);
+            $dataProvider->query->andFilterWhere(['between', 'created', $begin, $end]);
+
+        }
+
+
+        return $this->render('client_sales', [
+            'model' => $dataProvider->models,
+            'client' => $client
+        ]);
     }
 }
