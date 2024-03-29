47710a1c9542897c8cdbbb7500e413f67be8b186
VICTUS <yusupovdoniyor54@gmail.com>
2024-03-29 12:13:46 +0500

    1

--- frontend/config/params.php
+++ frontend/config/params.php
@@ -1,4 +1,8 @@
 <?php
 return [
     'adminEmail' => 'admin@example.com',
+    'payment_type_badge' => [
+        0 => '<span class="badge text-bg-success w-100">Приход <i class="bi bi-arrow-down-square"></i></span>',
+        1 => '<span class="badge text-bg-danger w-100">Расход <i class="bi bi-arrow-up-square"></i></span>',
+    ],
 ];
